f49003326c5a108df5fea60f29081c86f8836bbe
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-01-24 23:33:32 +0300

    добавлены комментарии

--- formatInput/FormatInput.go
+++ formatInput/FormatInput.go
@@ -2,21 +2,25 @@ package main
 
 import "fmt"
 
+// FormatInput function formatted output function
 func FormatInput() {
 	var number float64
 	_, err := fmt.Scan(&number)
     if err != nil {
         fmt.Println(err)
     }
-//	switch {
-//	case number <= 0:
-//		fmt.Printf("число %.2f не подходит\n", number)
-//	case number > 10000:
-//		fmt.Printf("%e\n", number)
-//	default:
-//		fmt.Printf("%.4f\n", number*number)
-//	}
 
+	// execution using the switch construct
+	switch {
+	case number <= 0:
+		fmt.Printf("число %.2f не подходит\n", number)
+	case number > 10000:
+		fmt.Printf("%e\n", number)
+	default:
+		fmt.Printf("%.4f\n", number*number)
+	}
+
+	// execution using the if construct
 	if number <= 0 {
         fmt.Printf("число %.2f не подходит\n", number)
 	} else if number > 10000 {
@@ -24,4 +28,8 @@ func FormatInput() {
 	} else {
         fmt.Printf("%.4f\n", number*number)
 	}
+}
+
+func main()  {
+	FormatInput()
 }
